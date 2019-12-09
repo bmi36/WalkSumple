bd50ca13ba8539d010e71c241d695dc6245fb61f
kyoto <kyoto2514@gmail.com>
2019-12-09 12:10:28 +0900

    センサー周り

--- app/src/main/java/com/example/walksumple/MainActivity.kt
+++ app/src/main/java/com/example/walksumple/MainActivity.kt
@@ -2,65 +2,52 @@ package com.example.walksumple
 
 import android.content.Context
 import android.hardware.Sensor
-import android.hardware.Sensor.TYPE_STEP_DETECTOR
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private lateinit var mSensorManager: SensorManager
-    private lateinit var mStepDetectorManager: Sensor
-    private lateinit var mStemCanterSensor: Sensor
+    private  var mSensorManager: SensorManager? = null
+    private var mStepDetectorSensor: Sensor? = null
+    private var mStepConterSensor: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-
-        mStepDetectorManager = mSensorManager.getDefaultSensor(TYPE_STEP_DETECTOR)
-
-        mStemCanterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        mStepDetectorSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)
+        mStepConterSensor = mSensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
     }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-//        accuracyに変更があったときの処理
-    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int){}
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        if (event != null) {
-            val sensor = event.sensor
-            val values = event.values
-            val timestamp = event.timestamp
-            if (sensor.type == Sensor.TYPE_STEP_COUNTER) {
-                textView.text = values[0].toString()
-            }
+    override fun onSensorChanged(event: SensorEvent) {
+        val sensor = event.sensor
+        val value = event.values
+        val teimeStamp = event.timestamp
+
+        if (sensor.type == Sensor.TYPE_STEP_COUNTER) {
+            Log.d("test", value[0].toString())
         }
     }
 
     override fun onResume() {
         super.onResume()
-        mSensorManager.registerListener(
-            this,
-            mStemCanterSensor,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
+        mSensorManager?.registerListener(this, mStepConterSensor, SensorManager.SENSOR_DELAY_NORMAL)
 
-        mSensorManager.registerListener(
-            this,
-            mStepDetectorManager,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-
+        mSensorManager?.registerListener(this,mStepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
-    override fun onPause() {
-        super.onPause()
-        mSensorManager.unregisterListener(this,mStemCanterSensor)
-        mSensorManager.unregisterListener(this,mStepDetectorManager)
+    override fun onStop() {
+        super.onStop()
+
+        mSensorManager?.unregisterListener(this,mStepConterSensor)
+        mSensorManager?.unregisterListener(this,mStepDetectorSensor)
     }
 }
